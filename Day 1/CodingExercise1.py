9fa5326da2843c7642b4b5cfe66d11dde002b5db
Sceptr3sec <prot0securesceptr3@gmail.com>
2026-05-04 21:50:33 -0500

    Day Directories and Day 1 Project

---
+++ Day 1/CodingExercise1.py
@@ -0,0 +1,7 @@
+###Printing Stuff
+
+print("Mix 500g of Flour with 300ml of Water in a bowl.")
+print("Knead the dough for 10 minutes.")
+print("Add 3g of Salt.")
+print("Leave to rise for 2 hours")
+print("Bake at 200 degrees Celsius for 30 minutes.")
