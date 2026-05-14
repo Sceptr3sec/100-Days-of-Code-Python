4fd801b4f0421a96fc78dfc839445e070b8d8f8f
Sceptr3sec <prot0securesceptr3@gmail.com>
2026-05-14 14:59:02 -0500

    Day 17

---
+++ Day 17/main.py
@@ -0,0 +1,12 @@
+import data
+import question_model
+import quiz_brain
+
+question_bank = []
+for question in data.question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = question_model.Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = quiz_brain.QuizBrain(question_bank)
